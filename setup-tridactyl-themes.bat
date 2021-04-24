ac4b160f68394aa57d7ec42a58bc840d7acdf7a7
Axel Garcia <ga419623@uaeh.edu.mx>
2021-04-24 15:37:13 -0500

    Adding tridactylrc plus batchfile to download the themes

---
+++ setup-tridactyl-themes.bat
@@ -0,0 +1,3 @@
+curl --create-dirs -o %USERPROFILE%/.config/tridactyl/themes/gruvbox-dark-hard.css https://raw.githubusercontent.com/bezmi/base16-tridactyl/master/base16-gruvbox-dark-hard.css
+curl --create-dirs -o %USERPROFILE%/.config/tridactyl/themes/gruvbox-light-hard.css https://raw.githubusercontent.com/bezmi/base16-tridactyl/master/base16-gruvbox-light-hard.css
+
